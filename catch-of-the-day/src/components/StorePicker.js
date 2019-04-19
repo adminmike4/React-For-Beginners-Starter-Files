9a276a95dd7272035a4b54c82d3982f152acc54b
adminmike4 <30911327+adminmike4@users.noreply.github.com>
2019-04-19 14:36:44 -0500

    11 Events, Refs and this Binding - lesson complete

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,34 @@ import React from 'react';
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+
+    myInput = React.createRef();
+
+    //If you need to access 'this' inide of a custom method you must use the syntax below or declare as above in a constructor.
+    goToStore = (event) => {
+        // 1 . Stop the form from submitting
+        event.preventDefault()
+        // 2. Get the text from that input (in this case using a ref)
+        console.log(this);
+
+        // 3. Change the page to /store/inputtext
+
+    }
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 {/*All JSX must be returned within one element */}
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+                <input 
+                    type="text"
+                    ref={this.myInput}
+                    required placeholder="Store Name" 
+                    defaultValue={getFunName()}
+                />
                 <button type="submit">Visit Store →</button>
                 {/* JSX comments must be placed as block comments in curly brackets */}
             </form>
@@ -15,4 +37,4 @@ class StorePicker extends React.Component {
     }
 }
 
-export default StorePicker;  
+export default StorePicker;
